4745f2df9ca5304eb11fb0611bc799b813391b55
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-10-18 21:42:20 +0530

    game class

---
+++ core/src/main/java/in/abhinavmishra/jchess/MainMenuScreen.java
@@ -0,0 +1,12 @@
+package in.abhinavmishra.jchess;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen {
+
+    final Drop game;
+
+    public MainMenuScreen(final Drop game) {
+        this.game = game;
+    }
+}
