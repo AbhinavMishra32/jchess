e1b62363b603d1e7e83c562c850a63e01625e969
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2025-10-29 01:04:38 +0530

    Knight moves fix!

--- core/src/main/java/in/abhinavmishra/jchess/pieces/Knight.java
+++ core/src/main/java/in/abhinavmishra/jchess/pieces/Knight.java
@@ -6,9 +6,9 @@ import in.abhinavmishra.jchess.Piece;
 import in.abhinavmishra.jchess.Square;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Knight extends Piece {
-
     public Knight(PieceColor pieceColor, String name, Texture texture, int row, int col, int x, int y, Board board) {
         super(pieceColor, name, texture, row, col, x, y, board);
     }
@@ -16,35 +16,25 @@ public class Knight extends Piece {
     @Override
     protected void setAllowedMoves() {
         ArrayList<int[]> moves = new ArrayList<>();
-        moves.add(new int[]{row, col});
 
-        if (isVisibleOnBoard(row + 2, col + 1)){
-            moves.add(new int[]{row + 2, col + 1});
-        }
-        if (isVisibleOnBoard(row + 1, col + 2)){
-            moves.add(new int[]{row + 1, col + 2});
-        }
-        if (isVisibleOnBoard(row - 1, col - 2)){
-            moves.add(new int[]{row - 1, col - 2});
-        }
-        if (isVisibleOnBoard(row - 2, col - 1)){
-            moves.add(new int[]{row - 2, col - 1});
-        }
-        if (isVisibleOnBoard(row + 2, col - 1)){
-            moves.add(new int[]{row + 2, col - 1});
-        }
-        if (isVisibleOnBoard(row + 1, col - 2)){
-            moves.add(new int[]{row + 1, col - 2});
-        }
-        if (isVisibleOnBoard(row - 1, col + 2)){
-            moves.add(new int[]{row - 1, col + 2});
-        }
-        if (isVisibleOnBoard(row - 2, col + 1)){
-            moves.add(new int[]{row - 2, col + 1});
+        int[][] offsets = {
+            { 2,  1}, { 1,  2}, {-1,  2}, {-2,  1},
+            {-2, -1}, {-1, -2}, { 1, -2}, { 2, -1}
+        };
+
+        for (int[] off : offsets) {
+            int newRow = row + off[0];
+            int newCol = col + off[1];
+
+            if (!isVisibleOnBoard(newRow, newCol)) continue;
+
+            Piece target = board.getPieceAt(newRow, newCol);
+            if (target == null || target.getPieceColor() != getPieceColor()) {
+                moves.add(new int[]{newRow, newCol});
+            }
         }
 
         allowedMoves = moves.toArray(new int[moves.size()][]);
-
     }
 
     @Override
